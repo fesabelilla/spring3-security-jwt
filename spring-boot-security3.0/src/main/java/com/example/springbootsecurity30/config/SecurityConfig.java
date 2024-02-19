0442c17ba46fb53542a9b73f3c9eb6ed4d2d7c3c
fesabelilla <fesabelilla@gmail.com>
2024-02-19 22:02:08 +0600

    Impl FilterChain | Authentication Impl done

--- spring-boot-security3.0/src/main/java/com/example/springbootsecurity30/config/SecurityConfig.java
+++ spring-boot-security3.0/src/main/java/com/example/springbootsecurity30/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springbootsecurity30.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +29,20 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/product/welcome").permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/product/**")
+                .authenticated()
+                .and()
+                .formLogin()
+                .and().build();
     }
 
     @Bean
